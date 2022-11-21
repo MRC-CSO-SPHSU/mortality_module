cfe5a6e70c3f56e57c256cf9998f5ff0c1d52d9e
Vladimir Khodygo <vladimir.khodygo@glasgow.ac.uk>
2022-11-21 12:37:53 +0000

    MAINT: drop/rearrange code

--- src/mortality_module/dynamics/ageing.py
+++ src/mortality_module/dynamics/ageing.py
@@ -1,10 +1,9 @@
-import typing
-
-from numpy import uint32, uint16, ceil
+from numpy import uint16, ceil, linspace
 from pandas import DataFrame, MultiIndex
 from itertools import product
 import mortality_module
 from mortality_module.utils.constants import AGE_GROUPS, SEX_LABELS
+import matplotlib.pylab as plt
 
 
 class Ageing:
@@ -13,28 +12,34 @@ class Ageing:
         population_pyramid: DataFrame,
         mortality_rates: DataFrame,
         birth_numbers: DataFrame,
+        start_year: uint16 = 2011,
         end_year: uint16 = 2020,
     ):
 
         self.pop = population_pyramid
-        self.mortality = mortality_rates
-        self.birth_numbers = birth_numbers
-        self.end_year = end_year
+        self._validate_population_pyramid()
 
-        self.index = [
-            (a, s, y)
-            for a, s, y in product(range(2011, (end_year + 2)), SEX_LABELS, AGE_GROUPS)
-        ]
+        self.mortality = mortality_rates
+        self._validate_mortality_rates()
+
+        self.birth_numbers = birth_numbers
+        self._validate_birth_numbers()
+
+        self.start_year = start_year
+        self.end_year = end_year
+        self._validate_dates()
+
+        indices = product(
+            range(self.start_year, (self.end_year + 2)), SEX_LABELS, AGE_GROUPS
+        )
+
+        self.index = [(age, sex, year_) for age, sex, year_ in indices]
+
         mi = MultiIndex.from_tuples(self.index, names=("year", "sex", "age_group"))
         columns = ["people_total", "mortality_rate", "dead_within_year"]
         self.df = DataFrame(index=mi, columns=columns)
 
-        self._validate_population_pyramid()
-        self._validate_mortality_rates()
-        self._validate_birth_numbers()
-        self._validate_end_year()
-
-    def _validate_end_year(self) -> None:
+    def _validate_dates(self) -> None:
         # 2011 and last year in mort table inclusive
         pass
 
@@ -53,29 +58,6 @@ class Ageing:
     def _add_new_people(self, df: DataFrame) -> DataFrame:
         pass
 
-    @staticmethod
-    def _calculate_dead_people(
-        df: DataFrame, mortality: DataFrame, year: str = "2011"
-    ) -> DataFrame:
-        dead = df.copy().drop("Number of people")
-        dead["Dead"] = 0.0
-
-        for s in SEX_LABELS:
-            for i in range(100):
-                age_value = str(i)
-                mr = mortality.loc[
-                    (mortality["year"] == year) & (mortality["sex"] == s), age_value
-                ]
-                dead.loc[(dead["Age"] == age_value) & (dead["sex"] == s), age_value] = (
-                    mr
-                    * df.loc[(dead["Age"] == age_value) & (dead["sex"] == s), age_value]
-                )
-
-        # treat 100 and over separately somehow?
-        # mortality tables: rows are years, columns are ages that year + sex
-
-        return dead
-
     def _remove_dead_people(self):
         pass
 
@@ -86,70 +68,34 @@ class Ageing:
         # add all mortality rates for all years
         self.df["mortality_rate"] = self.mortality["mortality_rate"]
 
-        for year in range(2011, (self.end_year + 1)):
+        for year_ in range(2011, (self.end_year + 1)):
             # calculate the number of dead people
             # do we round up or down?
-            dead = self.df.loc[year, ["people_total"]]
-            dead *= self.df.loc[year, ["mortality_rate"]].values
+            dead = self.df.loc[year_, ["people_total"]]
+            dead *= self.df.loc[year_, ["mortality_rate"]].values
 
             dead /= 100_000
             dead = dead.apply(lambda row: ceil(row)).astype("uint32")
-            self.df.loc[year, "dead_within_year"] = dead.values
+            self.df.loc[year_, "dead_within_year"] = dead.values
 
             # calculate pop - deaths next year and age it
             # at the moment all people who reach 100 die immediately.
-            pop_next_year = self.df.loc[year, ["people_total"]]
-            pop_next_year -= self.df.loc[year, ["dead_within_year"]].values
+            pop_next_year = self.df.loc[year_, ["people_total"]]
+            pop_next_year -= self.df.loc[year_, ["dead_within_year"]].values
             pop_next_year = pop_next_year.groupby(level=0).shift(1)
 
-            births_next_year = self.birth_numbers.loc[year]
+            births_next_year = self.birth_numbers.loc[year_]
             births_next_year.rename(
                 columns={"birth_number": "people_total"}, inplace=True
             )
 
             pop_next_year.update(births_next_year)
 
-            self.df.loc[[year + 1], "people_total"] = pop_next_year[
+            self.df.loc[[year_ + 1], "people_total"] = pop_next_year[
                 "people_total"
             ].values
 
         return self.df
-
-
-def ageing(df: DataFrame, birth_number: typing.Dict[str, uint32]) -> DataFrame:
-    """Ages every age group by one year.
-
-    Effectively shifts the data by 1 year, replacing the first row with the
-    number of newborns. In addition, the last age group that is `100 and over`
-    is a cumulative variable and needs to be updated separately.
-
-    :param df:
-    :param birth_number:
-    :return:
-    """
-
-    # validate_column_names(df)
-    # validate_column_dtypes(df)
-    # validate_age_groups(df)
-    # validate_sexes(df)
-    # validate_size(df)
-
-    # todo validate birth number
-
-    for s_value in SEX_LABELS:
-        old_elderly_value = df.loc[
-            df["Age group"] == "100 and over", df["Sex"] == s_value, "Number of people"
-        ]
-
-        df.loc[df["Sex"] == s_value] = df.loc[df["Sex"] == s_value].shift(
-            1, fill_value=birth_number[s_value]
-        )
-
-        df.loc[
-            df["Age group"] == "100 and over", df["Sex"] == s_value, "Number of people"
-        ] += old_elderly_value
-
-    return df
 
 
 if __name__ == "__main__":
@@ -160,19 +106,15 @@ if __name__ == "__main__":
     data = a.run()
     data.to_csv("result.csv")
 
-    import numpy as np
-    import matplotlib.pylab as pl
-
-    pl.figure()
-    colors = pl.cm.jet(np.linspace(0, 1, 11))
+    plt.figure()
+    colors = plt.cm.jet(linspace(0, 1, 11))
 
     for i, year in enumerate(range(2011, (2020 + 1))):
         data.loc[(year, "f", slice(None))]["people_total"].plot(
             color=colors[i], figsize=(32, 24), fontsize=26, label=str(year)
         )
 
-    pl.grid(axis="both", color="0.95")
-    pl.legend(title="Year of study:")
-    pl.title("Number of people per age group")
-    pl.savefig("result.png")
-    # pl.show()
+    plt.grid(axis="both", color="0.95")
+    plt.legend(title="Year of study:")
+    plt.title("Number of people per age group")
+    plt.savefig("result.png")
