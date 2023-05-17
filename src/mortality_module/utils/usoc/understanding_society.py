5f5df9c3ac68325163cb5ae61fca10745f47a042
Vladimir Khodygo <vladimir.khodygo@glasgow.ac.uk>
2023-05-17 17:30:13 +0100

    UPD: add method to read and merge all datafiles, income map

--- src/mortality_module/utils/usoc/understanding_society.py
+++ src/mortality_module/utils/usoc/understanding_society.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from typing import Final, List, Annotated, Dict
+from sklearn.preprocessing import MultiLabelBinarizer
 
 import pandas as pd
 
@@ -92,6 +93,173 @@ USOC_FIELDS_SPLIT: Final[Annotated[Dict[str, List[str]], 6]] = {
     'child': ['hidp', 'pidp', 'fnspid', 'mnspid', 'pns1pid', 'pns2pid',
               'depchl_dv', 'sex_dv'],
     'income': ['hidp', 'pidp', 'ficode']}
+
+FICODE: Final[Annotated[Dict[str, int], 36]] = {
+    'ni_retirement_state_retirement_old_age_pension': 1,
+    'a_pension_from_a_previous_employer': 2,
+    'a_pension_from_a_spouses_previous_employer': 3,
+    'a_private_pension_annuity': 4,
+    'a_widows_or_war_widows_pension': 5,
+    'a_widowed_mothers_allowance_widowed_parents_allowance_bereavement_allowance': 6,
+    'pension_credit_includes_guarantee_credit_and_saving_credit': 7,
+    'severe_disablement_allowance': 8,
+    'industrial_injury_disablement_allowance': 9,
+    'disability_living_allowance': 10,
+    'attendance_allowance': 11,
+    'carers_allowance_formerly_invalid_care_allowance': 12,
+    'war_disablement_pension': 13,
+    'incapacity_benefit': 14,
+    'income_support': 15,
+    'job_seekers_allowance': 16,
+    'national_insurance_credits': 17,
+    'child_benefit_including_lone_parent_child_benefit_payments': 18,
+    'child_tax_credit': 19,
+    'working_tax_credit_includes_disabled_persons_tax_credit': 20,
+    'maternity_allowance': 21,
+    'housing_benefit': 22,
+    'council_tax_benefit': 23,
+    'educational_grant_not_student_loan_or_tuition_fee_loan': 24,
+    'trade_union_friendly_society_payment': 25,
+    'maintenance_or_alimony': 26,
+    'payments_from_a_family_member_not_living_here': 27,
+    'rent_from_boarders_or_lodgers_not_family_members_living_here_with_you': 28,
+    'rent_from_any_other_property': 29,
+    'foster_allowance_guardian_allowance': 30,
+    'rent_rebate': 31,
+    'rate_rebate': 32,
+    'employment_and_support_allowance': 33,
+    'return_to_work_credit': 34,
+    'sickness_and_accident_insurance': 35,
+    'in_work_credit_for_lone_parents': 36}
+
+
+def merge_usoc_data(path_: str | Path = None) -> pd.DataFrame:
+    """ Combines all provided USoc files into a single table of individuals.
+
+    A raw data pre-processor, this method merges multiple tables together using
+    household and person ids as keys. In the case when field values differ
+    individual responses take priority.
+
+    Parameters
+    ----------
+        path_ : str or Path
+            The path to a directory containing .csv files, `None` by default.
+
+    Returns
+    -------
+    pd.DataFrame
+        A single dataframe with all required individual data.
+
+    """
+    p_ = path_validation(path_)
+
+    def load_file(filename: str) -> pd.DataFrame:
+        """Reads a .csv file and sets indices to 'pidp' and 'hidp'.
+
+        Parameters
+        ----------
+        filename : str
+            The name of a file.
+
+        Returns
+        -------
+        pd.DataFrame
+            A dataframe.
+        """
+        f = pd.read_csv(p_ / (filename + '.csv'),
+                        usecols=USOC_FIELDS_SPLIT[filename],
+                        low_memory=False
+                        )
+        i_ = ['pidp', 'hidp'] if {'pidp',
+                                  'hidp'} <= set(f.columns) else ['hidp']
+        f[i_] = f[i_].astype(int)
+        return f.set_index(i_)
+
+    data = load_file('indall')
+
+    def find_merge_divergent_data(other_file: str) -> pd.DataFrame:
+        """ A helper function to reduce copy-paste code, does the merging.
+
+        Combines two files into one while making sure that individual responses
+        take priority over the same fields but with different values.
+
+
+        Parameters
+        ----------
+        other_file : str
+            The name of a file
+
+        Returns
+        -------
+        pd.DataFrame
+            The result of merging and override.
+
+        """
+        new_df = load_file(other_file)
+
+        common_fields = list(set(data.columns) & set(new_df.columns))
+        for f_ in common_fields:
+            mask_ = (data.loc[new_df.index, f_] == new_df[f_])
+            if not all(mask_):
+                print(f'Individual responses take precedence, replacing {f_}')
+                not_mask_ = ~mask_
+                data.loc[new_df.index, f_][not_mask_] = new_df[f_][not_mask_]
+
+        new_fields = list(set(new_df.columns) - set(common_fields))
+        data.loc[new_df.index, new_fields] = new_df[new_fields]
+        if len(new_fields) > 0:
+            data.loc[new_df.index, new_fields] = new_df[new_fields]
+
+        return data
+
+    data = find_merge_divergent_data('indresp')
+    data = find_merge_divergent_data('youth')
+    data = find_merge_divergent_data('child')
+
+    income = load_file('income')
+    income['ficode'] = (income['ficode']
+                        .apply(lambda l: l.lower())
+                        .apply(lambda l: l.replace(' / ', '/'))
+                        .apply(lambda l: l.replace(' ', '_'))
+                        .apply(lambda l: l.replace('-', '_'))
+                        .apply(lambda l: l.replace('(', ''))
+                        .apply(lambda l: l.replace(')', ''))
+                        .apply(lambda l: l.replace('\'', ''))
+                        .apply(lambda l: l.replace('/', '_'))
+                        .apply(lambda l: l.replace('&amp;', 'and'))
+                        .apply(lambda l: l.replace('__', '_'))
+                        )
+
+    income = (income
+              .replace({'ficode': FICODE})
+              .groupby(['hidp', 'pidp'])['ficode']
+              .apply(list)
+              .reset_index()
+              .set_index(['hidp', 'pidp'])
+              )
+
+    mlb = MultiLabelBinarizer()
+    income = pd.DataFrame(mlb.fit_transform(income.pop('ficode')),
+                          columns=[f'ficode_{v}' for v in mlb.classes_],
+                          index=income.index
+                          )
+
+    data = (data.merge(income,
+                       how='left',
+                       left_on=['pidp', 'hidp'],
+                       right_on=['pidp', 'hidp'])
+            )
+
+    for i in range(1, len(FICODE) + 1):
+        data[f'ficode_{i}'] = data[f'ficode_{i}'].fillna(0).astype(bool)
+
+    household_response = load_file('hhresp')
+    data = (data.merge(household_response,
+                       how='left',
+                       left_on=['hidp'],
+                       right_on=['hidp'])
+            )
+    return data
 
 
 def _read_usoc_fields(path_: str | Path = None,
